2dbcff8dc283cfd349e34f118099ec4c28b4caca
gestordeveloper <109088147+gestordeveloper@users.noreply.github.com>
2024-09-24 13:59:41 -0300

    adicionando icons e propriedades de texto

---
+++ icons/icons-dist/icons.ts
@@ -0,0 +1,577 @@
+export type IconsId =
+  | "arrow-1"
+  | "arrow-10"
+  | "arrow-11"
+  | "arrow-12"
+  | "arrow-13"
+  | "arrow-15"
+  | "arrow-16"
+  | "arrow-17"
+  | "arrow-18"
+  | "arrow-19"
+  | "arrow-2"
+  | "arrow-20"
+  | "arrow-21"
+  | "arrow-22"
+  | "arrow-3"
+  | "arrow-5"
+  | "arrow-7"
+  | "arrow-9"
+  | "box-1"
+  | "box-2"
+  | "box-3"
+  | "box-4"
+  | "box-5"
+  | "box-6"
+  | "box-7"
+  | "box-8"
+  | "calendar-1"
+  | "calendar-2"
+  | "calendar-3"
+  | "calendar-4"
+  | "calendar-5"
+  | "calendar-6"
+  | "calendar-7"
+  | "calendar-8"
+  | "cancel-1"
+  | "cancel-2"
+  | "chart-1"
+  | "chart-10"
+  | "chart-11"
+  | "chart-12"
+  | "chart-13"
+  | "chart-14"
+  | "chart-15"
+  | "chart-16"
+  | "chart-2"
+  | "chart-3"
+  | "chart-4"
+  | "chart-5"
+  | "chart-6"
+  | "chart-7"
+  | "chart-8"
+  | "chart-9"
+  | "clock-1"
+  | "clock-2"
+  | "clock-3"
+  | "config-1"
+  | "config-2"
+  | "config-3"
+  | "config-4"
+  | "config-5"
+  | "config-6"
+  | "config-7"
+  | "config-8"
+  | "config-9"
+  | "edit-1"
+  | "edit-2"
+  | "house-1"
+  | "kit-1"
+  | "kit-10"
+  | "kit-11"
+  | "kit-12"
+  | "kit-13"
+  | "kit-14"
+  | "kit-15"
+  | "kit-16"
+  | "kit-17"
+  | "kit-18"
+  | "kit-19"
+  | "kit-2"
+  | "kit-20"
+  | "kit-21"
+  | "kit-22"
+  | "kit-23"
+  | "kit-24"
+  | "kit-25"
+  | "kit-26"
+  | "kit-27"
+  | "kit-28"
+  | "kit-29"
+  | "kit-3"
+  | "kit-30"
+  | "kit-31"
+  | "kit-32"
+  | "kit-33"
+  | "kit-34"
+  | "kit-35"
+  | "kit-36"
+  | "kit-4"
+  | "kit-5"
+  | "kit-6"
+  | "kit-7"
+  | "kit-8"
+  | "kit-9"
+  | "mail-1"
+  | "money-1"
+  | "money-2"
+  | "money-3"
+  | "money-4"
+  | "money-5"
+  | "money-6"
+  | "money-7"
+  | "money-8"
+  | "money-9"
+  | "plus-1"
+  | "plus-2"
+  | "print-1"
+  | "search-1"
+  | "social-1"
+  | "social-2"
+  | "social-3"
+  | "social-4"
+  | "social-5"
+  | "social-6"
+  | "social-7"
+  | "social-8"
+  | "star-1"
+  | "success-1"
+  | "success-2"
+  | "success-3"
+  | "trash-1"
+  | "trash-2"
+  | "trash-3"
+  | "user-1"
+  | "user-10"
+  | "user-2"
+  | "user-3"
+  | "user-4"
+  | "user-5"
+  | "user-6"
+  | "user-7"
+  | "user-8"
+  | "user-9";
+
+export type IconsKey =
+  | "Arrow_1"
+  | "Arrow_10"
+  | "Arrow_11"
+  | "Arrow_12"
+  | "Arrow_13"
+  | "Arrow_15"
+  | "Arrow_16"
+  | "Arrow_17"
+  | "Arrow_18"
+  | "Arrow_19"
+  | "Arrow_2"
+  | "Arrow_20"
+  | "Arrow_21"
+  | "Arrow_22"
+  | "Arrow_3"
+  | "Arrow_5"
+  | "Arrow_7"
+  | "Arrow_9"
+  | "Box_1"
+  | "Box_2"
+  | "Box_3"
+  | "Box_4"
+  | "Box_5"
+  | "Box_6"
+  | "Box_7"
+  | "Box_8"
+  | "Calendar_1"
+  | "Calendar_2"
+  | "Calendar_3"
+  | "Calendar_4"
+  | "Calendar_5"
+  | "Calendar_6"
+  | "Calendar_7"
+  | "Calendar_8"
+  | "Cancel_1"
+  | "Cancel_2"
+  | "Chart_1"
+  | "Chart_10"
+  | "Chart_11"
+  | "Chart_12"
+  | "Chart_13"
+  | "Chart_14"
+  | "Chart_15"
+  | "Chart_16"
+  | "Chart_2"
+  | "Chart_3"
+  | "Chart_4"
+  | "Chart_5"
+  | "Chart_6"
+  | "Chart_7"
+  | "Chart_8"
+  | "Chart_9"
+  | "Clock_1"
+  | "Clock_2"
+  | "Clock_3"
+  | "Config_1"
+  | "Config_2"
+  | "Config_3"
+  | "Config_4"
+  | "Config_5"
+  | "Config_6"
+  | "Config_7"
+  | "Config_8"
+  | "Config_9"
+  | "Edit_1"
+  | "Edit_2"
+  | "House_1"
+  | "Kit_1"
+  | "Kit_10"
+  | "Kit_11"
+  | "Kit_12"
+  | "Kit_13"
+  | "Kit_14"
+  | "Kit_15"
+  | "Kit_16"
+  | "Kit_17"
+  | "Kit_18"
+  | "Kit_19"
+  | "Kit_2"
+  | "Kit_20"
+  | "Kit_21"
+  | "Kit_22"
+  | "Kit_23"
+  | "Kit_24"
+  | "Kit_25"
+  | "Kit_26"
+  | "Kit_27"
+  | "Kit_28"
+  | "Kit_29"
+  | "Kit_3"
+  | "Kit_30"
+  | "Kit_31"
+  | "Kit_32"
+  | "Kit_33"
+  | "Kit_34"
+  | "Kit_35"
+  | "Kit_36"
+  | "Kit_4"
+  | "Kit_5"
+  | "Kit_6"
+  | "Kit_7"
+  | "Kit_8"
+  | "Kit_9"
+  | "Mail_1"
+  | "Money_1"
+  | "Money_2"
+  | "Money_3"
+  | "Money_4"
+  | "Money_5"
+  | "Money_6"
+  | "Money_7"
+  | "Money_8"
+  | "Money_9"
+  | "Plus_1"
+  | "Plus_2"
+  | "Print_1"
+  | "Search_1"
+  | "Social_1"
+  | "Social_2"
+  | "Social_3"
+  | "Social_4"
+  | "Social_5"
+  | "Social_6"
+  | "Social_7"
+  | "Social_8"
+  | "Star_1"
+  | "Success_1"
+  | "Success_2"
+  | "Success_3"
+  | "Trash_1"
+  | "Trash_2"
+  | "Trash_3"
+  | "User_1"
+  | "User_10"
+  | "User_2"
+  | "User_3"
+  | "User_4"
+  | "User_5"
+  | "User_6"
+  | "User_7"
+  | "User_8"
+  | "User_9";
+
+export enum Icons {
+  Arrow_1 = "arrow-1",
+  Arrow_10 = "arrow-10",
+  Arrow_11 = "arrow-11",
+  Arrow_12 = "arrow-12",
+  Arrow_13 = "arrow-13",
+  Arrow_15 = "arrow-15",
+  Arrow_16 = "arrow-16",
+  Arrow_17 = "arrow-17",
+  Arrow_18 = "arrow-18",
+  Arrow_19 = "arrow-19",
+  Arrow_2 = "arrow-2",
+  Arrow_20 = "arrow-20",
+  Arrow_21 = "arrow-21",
+  Arrow_22 = "arrow-22",
+  Arrow_3 = "arrow-3",
+  Arrow_5 = "arrow-5",
+  Arrow_7 = "arrow-7",
+  Arrow_9 = "arrow-9",
+  Box_1 = "box-1",
+  Box_2 = "box-2",
+  Box_3 = "box-3",
+  Box_4 = "box-4",
+  Box_5 = "box-5",
+  Box_6 = "box-6",
+  Box_7 = "box-7",
+  Box_8 = "box-8",
+  Calendar_1 = "calendar-1",
+  Calendar_2 = "calendar-2",
+  Calendar_3 = "calendar-3",
+  Calendar_4 = "calendar-4",
+  Calendar_5 = "calendar-5",
+  Calendar_6 = "calendar-6",
+  Calendar_7 = "calendar-7",
+  Calendar_8 = "calendar-8",
+  Cancel_1 = "cancel-1",
+  Cancel_2 = "cancel-2",
+  Chart_1 = "chart-1",
+  Chart_10 = "chart-10",
+  Chart_11 = "chart-11",
+  Chart_12 = "chart-12",
+  Chart_13 = "chart-13",
+  Chart_14 = "chart-14",
+  Chart_15 = "chart-15",
+  Chart_16 = "chart-16",
+  Chart_2 = "chart-2",
+  Chart_3 = "chart-3",
+  Chart_4 = "chart-4",
+  Chart_5 = "chart-5",
+  Chart_6 = "chart-6",
+  Chart_7 = "chart-7",
+  Chart_8 = "chart-8",
+  Chart_9 = "chart-9",
+  Clock_1 = "clock-1",
+  Clock_2 = "clock-2",
+  Clock_3 = "clock-3",
+  Config_1 = "config-1",
+  Config_2 = "config-2",
+  Config_3 = "config-3",
+  Config_4 = "config-4",
+  Config_5 = "config-5",
+  Config_6 = "config-6",
+  Config_7 = "config-7",
+  Config_8 = "config-8",
+  Config_9 = "config-9",
+  Edit_1 = "edit-1",
+  Edit_2 = "edit-2",
+  House_1 = "house-1",
+  Kit_1 = "kit-1",
+  Kit_10 = "kit-10",
+  Kit_11 = "kit-11",
+  Kit_12 = "kit-12",
+  Kit_13 = "kit-13",
+  Kit_14 = "kit-14",
+  Kit_15 = "kit-15",
+  Kit_16 = "kit-16",
+  Kit_17 = "kit-17",
+  Kit_18 = "kit-18",
+  Kit_19 = "kit-19",
+  Kit_2 = "kit-2",
+  Kit_20 = "kit-20",
+  Kit_21 = "kit-21",
+  Kit_22 = "kit-22",
+  Kit_23 = "kit-23",
+  Kit_24 = "kit-24",
+  Kit_25 = "kit-25",
+  Kit_26 = "kit-26",
+  Kit_27 = "kit-27",
+  Kit_28 = "kit-28",
+  Kit_29 = "kit-29",
+  Kit_3 = "kit-3",
+  Kit_30 = "kit-30",
+  Kit_31 = "kit-31",
+  Kit_32 = "kit-32",
+  Kit_33 = "kit-33",
+  Kit_34 = "kit-34",
+  Kit_35 = "kit-35",
+  Kit_36 = "kit-36",
+  Kit_4 = "kit-4",
+  Kit_5 = "kit-5",
+  Kit_6 = "kit-6",
+  Kit_7 = "kit-7",
+  Kit_8 = "kit-8",
+  Kit_9 = "kit-9",
+  Mail_1 = "mail-1",
+  Money_1 = "money-1",
+  Money_2 = "money-2",
+  Money_3 = "money-3",
+  Money_4 = "money-4",
+  Money_5 = "money-5",
+  Money_6 = "money-6",
+  Money_7 = "money-7",
+  Money_8 = "money-8",
+  Money_9 = "money-9",
+  Plus_1 = "plus-1",
+  Plus_2 = "plus-2",
+  Print_1 = "print-1",
+  Search_1 = "search-1",
+  Social_1 = "social-1",
+  Social_2 = "social-2",
+  Social_3 = "social-3",
+  Social_4 = "social-4",
+  Social_5 = "social-5",
+  Social_6 = "social-6",
+  Social_7 = "social-7",
+  Social_8 = "social-8",
+  Star_1 = "star-1",
+  Success_1 = "success-1",
+  Success_2 = "success-2",
+  Success_3 = "success-3",
+  Trash_1 = "trash-1",
+  Trash_2 = "trash-2",
+  Trash_3 = "trash-3",
+  User_1 = "user-1",
+  User_10 = "user-10",
+  User_2 = "user-2",
+  User_3 = "user-3",
+  User_4 = "user-4",
+  User_5 = "user-5",
+  User_6 = "user-6",
+  User_7 = "user-7",
+  User_8 = "user-8",
+  User_9 = "user-9",
+}
+
+export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
+  [Icons.Arrow_1]: "61697",
+  [Icons.Arrow_10]: "61698",
+  [Icons.Arrow_11]: "61699",
+  [Icons.Arrow_12]: "61700",
+  [Icons.Arrow_13]: "61701",
+  [Icons.Arrow_15]: "61702",
+  [Icons.Arrow_16]: "61703",
+  [Icons.Arrow_17]: "61704",
+  [Icons.Arrow_18]: "61705",
+  [Icons.Arrow_19]: "61706",
+  [Icons.Arrow_2]: "61707",
+  [Icons.Arrow_20]: "61708",
+  [Icons.Arrow_21]: "61709",
+  [Icons.Arrow_22]: "61710",
+  [Icons.Arrow_3]: "61711",
+  [Icons.Arrow_5]: "61712",
+  [Icons.Arrow_7]: "61713",
+  [Icons.Arrow_9]: "61714",
+  [Icons.Box_1]: "61715",
+  [Icons.Box_2]: "61716",
+  [Icons.Box_3]: "61717",
+  [Icons.Box_4]: "61718",
+  [Icons.Box_5]: "61719",
+  [Icons.Box_6]: "61720",
+  [Icons.Box_7]: "61721",
+  [Icons.Box_8]: "61722",
+  [Icons.Calendar_1]: "61723",
+  [Icons.Calendar_2]: "61724",
+  [Icons.Calendar_3]: "61725",
+  [Icons.Calendar_4]: "61726",
+  [Icons.Calendar_5]: "61727",
+  [Icons.Calendar_6]: "61728",
+  [Icons.Calendar_7]: "61729",
+  [Icons.Calendar_8]: "61730",
+  [Icons.Cancel_1]: "61731",
+  [Icons.Cancel_2]: "61732",
+  [Icons.Chart_1]: "61733",
+  [Icons.Chart_10]: "61734",
+  [Icons.Chart_11]: "61735",
+  [Icons.Chart_12]: "61736",
+  [Icons.Chart_13]: "61737",
+  [Icons.Chart_14]: "61738",
+  [Icons.Chart_15]: "61739",
+  [Icons.Chart_16]: "61740",
+  [Icons.Chart_2]: "61741",
+  [Icons.Chart_3]: "61742",
+  [Icons.Chart_4]: "61743",
+  [Icons.Chart_5]: "61744",
+  [Icons.Chart_6]: "61745",
+  [Icons.Chart_7]: "61746",
+  [Icons.Chart_8]: "61747",
+  [Icons.Chart_9]: "61748",
+  [Icons.Clock_1]: "61749",
+  [Icons.Clock_2]: "61750",
+  [Icons.Clock_3]: "61751",
+  [Icons.Config_1]: "61752",
+  [Icons.Config_2]: "61753",
+  [Icons.Config_3]: "61754",
+  [Icons.Config_4]: "61755",
+  [Icons.Config_5]: "61756",
+  [Icons.Config_6]: "61757",
+  [Icons.Config_7]: "61758",
+  [Icons.Config_8]: "61759",
+  [Icons.Config_9]: "61760",
+  [Icons.Edit_1]: "61761",
+  [Icons.Edit_2]: "61762",
+  [Icons.House_1]: "61763",
+  [Icons.Kit_1]: "61764",
+  [Icons.Kit_10]: "61765",
+  [Icons.Kit_11]: "61766",
+  [Icons.Kit_12]: "61767",
+  [Icons.Kit_13]: "61768",
+  [Icons.Kit_14]: "61769",
+  [Icons.Kit_15]: "61770",
+  [Icons.Kit_16]: "61771",
+  [Icons.Kit_17]: "61772",
+  [Icons.Kit_18]: "61773",
+  [Icons.Kit_19]: "61774",
+  [Icons.Kit_2]: "61775",
+  [Icons.Kit_20]: "61776",
+  [Icons.Kit_21]: "61777",
+  [Icons.Kit_22]: "61778",
+  [Icons.Kit_23]: "61779",
+  [Icons.Kit_24]: "61780",
+  [Icons.Kit_25]: "61781",
+  [Icons.Kit_26]: "61782",
+  [Icons.Kit_27]: "61783",
+  [Icons.Kit_28]: "61784",
+  [Icons.Kit_29]: "61785",
+  [Icons.Kit_3]: "61786",
+  [Icons.Kit_30]: "61787",
+  [Icons.Kit_31]: "61788",
+  [Icons.Kit_32]: "61789",
+  [Icons.Kit_33]: "61790",
+  [Icons.Kit_34]: "61791",
+  [Icons.Kit_35]: "61792",
+  [Icons.Kit_36]: "61793",
+  [Icons.Kit_4]: "61794",
+  [Icons.Kit_5]: "61795",
+  [Icons.Kit_6]: "61796",
+  [Icons.Kit_7]: "61797",
+  [Icons.Kit_8]: "61798",
+  [Icons.Kit_9]: "61799",
+  [Icons.Mail_1]: "61800",
+  [Icons.Money_1]: "61801",
+  [Icons.Money_2]: "61802",
+  [Icons.Money_3]: "61803",
+  [Icons.Money_4]: "61804",
+  [Icons.Money_5]: "61805",
+  [Icons.Money_6]: "61806",
+  [Icons.Money_7]: "61807",
+  [Icons.Money_8]: "61808",
+  [Icons.Money_9]: "61809",
+  [Icons.Plus_1]: "61810",
+  [Icons.Plus_2]: "61811",
+  [Icons.Print_1]: "61812",
+  [Icons.Search_1]: "61813",
+  [Icons.Social_1]: "61814",
+  [Icons.Social_2]: "61815",
+  [Icons.Social_3]: "61816",
+  [Icons.Social_4]: "61817",
+  [Icons.Social_5]: "61818",
+  [Icons.Social_6]: "61819",
+  [Icons.Social_7]: "61820",
+  [Icons.Social_8]: "61821",
+  [Icons.Star_1]: "61822",
+  [Icons.Success_1]: "61823",
+  [Icons.Success_2]: "61824",
+  [Icons.Success_3]: "61825",
+  [Icons.Trash_1]: "61826",
+  [Icons.Trash_2]: "61827",
+  [Icons.Trash_3]: "61828",
+  [Icons.User_1]: "61829",
+  [Icons.User_10]: "61830",
+  [Icons.User_2]: "61831",
+  [Icons.User_3]: "61832",
+  [Icons.User_4]: "61833",
+  [Icons.User_5]: "61834",
+  [Icons.User_6]: "61835",
+  [Icons.User_7]: "61836",
+  [Icons.User_8]: "61837",
+  [Icons.User_9]: "61838",
+};
